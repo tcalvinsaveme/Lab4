4d3d65c1f353128613aac6f8f2ffd04b509608c0
Hadi <belkasri@umich.edu>
2020-02-06 15:41:25 -0500

    Fixed error

--- Hello.java
+++ Hello.java
@@ -1,8 +1,7 @@
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        //Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+public class Hello {
+  public static void main(String[] args) {
+    for(int i=1;i<=10;i++) {
+      System.out.println("Hello World..."+i);
     }
-
+  }
 }
